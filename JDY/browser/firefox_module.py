2a9e9b51f98818a39994284df23b43f01223b7a4
lijie.xu <lijie.xu@soooqooo.com>
2018-03-15 15:27:20 +0800

    2

--- JDY/browser/firefox_module.py
+++ JDY/browser/firefox_module.py
@@ -1,7 +1,6 @@
 #  -*- coding: utf-8 -*-
 import os
 import sys
-
 __project_dir__ = os.path.dirname(os.path.dirname(os.path.realpath(__file__)))
 if __project_dir__ not in sys.path:
     sys.path.append(__project_dir__)
