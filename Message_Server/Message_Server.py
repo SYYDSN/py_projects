6219e6b0fac5f30b936dfa9f4a99839b364328a1
lijie.xu <583736361@qq.com>
2018-03-29 07:15:37 +0800

    2

--- Message_Server/Message_Server.py
+++ Message_Server/Message_Server.py
@@ -10,6 +10,7 @@ from werkzeug.contrib.cache import RedisCache
 from flask_session import Session
 from log_module import get_logger
 import sms_module
+import json
 from tools_module import *
 import os
 
@@ -27,9 +28,49 @@ logger = get_logger()
 port = 8000
 
 
-@app.route('/')
+def get_signature(nonce, payload, secret, timestamp):
+    """生成简道云的签名验证"""
+    content = ':'.join([nonce, payload, secret, timestamp]).encode('utf-8')
+    m = hashlib.sha1()
+    m.update(content)
+    return m.hexdigest()
+
+
+def validate_signature(req, secret, signature) -> bool:
+    """验证简道云发来的消息的签名是否正确？"""
+    payload = req.data.decode('utf-8')
+    nonce = req.args['nonce']
+    timestamp = req.args['timestamp']
+    if signature != get_signature(nonce, payload, secret, timestamp):
+        return False
+    else:
+        return True
+
+
+@app.route('/', methods=['post', 'get'])
 def hello_world():
     return 'Hello World!'
+
+
+@app.route("/listen_<key>", methods=['post'])
+def listen_func(key):
+    """监听简道云发送过来的消息"""
+    mes = {"message":"success"}
+    headers = request.headers
+    event_id = headers.get("X-JDY-DeliverId")
+    signature = headers.get("X-JDY-Signature")
+    data = request.json
+    print(event_id)
+    print(signature)
+    print(data)
+    if key == "test":
+        """测试消息"""
+        secret_str = "ckFqpdtIr45aXwPkSITuW2iY"  # 不同的消息定义的secret不同，用来验证消息的合法性
+        print(validate_signature(request, secret_str, signature))
+    else:
+        mes['message'] = '错误的path'
+    return json.dumps(mes)
+
 
 
 @app.before_request
@@ -39,9 +80,13 @@ def logger_request_info():
     :return:
     """
     headers = "headers: {}".format(request.headers)
-    args_dict = "args: {}".format(get_args(request))
+    args = "args: {}".format(request.args)
+    form = "form: {}".format(request.form)
+    json = "json: {}".format(request.json)
     logger.info(headers)
-    logger.info(args_dict)
+    logger.info(args)
+    logger.info(form)
+    logger.info(json)
 
 
 if __name__ == '__main__':
