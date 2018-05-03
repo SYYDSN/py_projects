3c15e055a27162532fddb0116327847c2e9869b1
lijie.xu <583736361@qq.com>
2018-05-04 07:55:42 +0800

    1

--- Draw_Server/module/excel_module.py
+++ Draw_Server/module/excel_module.py
@@ -84,5 +84,80 @@ def read_excel(file_path: str):
     return res
 
 
+def calculate(p_path):
+    excel = openpyxl.load_workbook(p_path)
+    sheets = excel.sheetnames
+    res = list()
+    for sheet in sheets:
+        sheet = excel.get_sheet_by_name(sheet)
+        for i, tr in enumerate(sheet):
+            if i == 0:
+                pass
+            else:
+                init = {
+                    "ticket": tr[0].value,
+                    "m4_account": tr[1].value,
+                    "platform": tr[2].value,
+                    "customer_name": tr[3].value,
+                    "command": tr[4].value,
+                    "lot": tr[6].value,
+                    "close_date": tr[8].value,
+                    "swap": tr[9].value,
+                    "commission": tr[10].value,
+                    "profit": tr[11].value,
+                    "spread_profit": tr[12].value,
+                    "sales_name": tr[13].value,
+                    "manager_name": tr[14].value,
+                    "director_name": tr[15].value
+                }
+                res.append(init)
+    res.sort(key=lambda obj: [obj['close_date']], reverse=True)
+    """打印总手数"""
+    lot_count = sum([x['lot'] for x in res if isinstance(x['lot'], (float, int))])
+    print("总手数:{}".format(lot_count))
+    return res
+
+
+def calculate2(p_path):
+    excel = openpyxl.load_workbook(p_path)
+    sheets = excel.sheetnames
+    res = list()
+    for sheet in sheets:
+        sheet = excel.get_sheet_by_name(sheet)
+        for i, tr in enumerate(sheet):
+            if i == 0:
+                pass
+            else:
+                init = {
+                    "ticket": int(tr[0].value) if tr[0].value.isdigit() else tr[0].value,
+                    "m4_account": tr[1].value,
+                    "command": tr[3].value,
+                    "lot": float(tr[4].value) if tr[4].value is not None else 0,
+                    "close_date": tr[7].value,
+                    "swap": float(tr[10].value) if tr[10].value is not None else 0,
+                    "commission": float(tr[11].value) if tr[11].value is not None else 0,
+                    "profit": float(tr[9].value) if tr[9].value is not None else 0
+                }
+                if init['close_date'] is not None:
+                    res.append(init)
+    res.sort(key=lambda obj: [obj['close_date']], reverse=True)
+    """打印总手数"""
+    lot_count = sum([x['lot'] for x in res if isinstance(x['lot'], (float, int))])
+    print("总手数:{}".format(lot_count))
+    return res
+
+
 if __name__ == "__main__":
+    p1 = "/home/walle/work/projects/Draw_Server/module/temp_file/2018-04-01至2018-05-01交易报表.xlsx"
+    p2 = "/home/walle/work/projects/Draw_Server/module/temp_file/交易记录2018-05-03 12_02_52.xlsx"
+    r1 = calculate(p1)
+    r2 = calculate2(p2)
+    print(len(r1))
+    print(len(r2))
+    print(len(r1) - len(r2))
+    r2_keys = [x['ticket'] for x in r2]
+    aa = [x for x in r1 if x['ticket'] not in r2_keys]
+    print(len(aa))
+    for x in aa:
+        print(x)
     pass
