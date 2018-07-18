3178b2f7e26315d09dcb3b867ad4d614b87b9697
lijie.xu <583736361@qq.com>
2018-07-19 07:49:05 +0800

    1

--- t1/js/index.js
+++ t1/js/index.js
@@ -3,9 +3,12 @@ $(function(){
     $(".text,.text_2").each(function(){
         var $this = $(this);
         $this.hover(function(){
-            $this.css("color", "#8cb4e0").prev().attr("src","image/blue_border.png");
+            $this.css("color", "#8cb4e0");
+            $this.prev().prev().hide(0).next(".b_02:first").show(0);
+            console.log($this.html());
+            console.log($this.prev(".b_01:first")[0]);
         },function(){
-        $this.css("color", "#434343").prev().attr("src","image/red_border.png");
+        $this.css("color", "#434343").prev(".b_02").hide(0).prev(".b_01").show(0);
     });
     });
 
