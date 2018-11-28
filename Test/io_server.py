20ea4351805474eeb59311188025af0a5d40172f
walle <583736361@qq.com>
2018-11-28 18:36:33 +0800

    1

---
+++ Test/io_server.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+from uuid import uuid4
+from flask import Flask, render_template
+from flask_socketio import SocketIO
+from flask_socketio import emit
+
+
+"""socketio注意客户端必须用本项目中的socket.io.js,否则不支持中文"""
+
+app = Flask(__name__)
+port = 8100
+secret = uuid4().hex
+app.config['SECRET_KEY'] = secret
+socketio = SocketIO(app)
+
+
+@app.route("/io_client")
+def io_client_func():
+    return render_template("io_client.html")
+
+@socketio.on('mes')
+def handle_my_custom_event(json_str):
+    print('received json: ' + str(json_str))
+    emit("mes", json_str, broadcast=True)
+
+
+
+if __name__ == '__main__':
+    socketio.run(app, port=port, host="0.0.0.0")
+    pass
---
+++ Test/static/js/io_client.js
@@ -0,0 +1,37 @@
+var socket = io.connect('http://' + document.domain + ':' + location.port);
+socket.on('connect', function() {
+    socket.emit('mes', {data: 'I\'m 中 connected!'});
+});
+
+socket.on('mes', function(json){;
+    console.log(json);
+    var str = json['message'];
+    if(str !== undefined && str !== ""){
+        var div = $(`<div>${str}</div>`);
+        $(".show").append(div);
+    }
+
+});
+
+nick_name = prompt("请输入你的昵称");
+while($.trim(nick_name) === ""){
+    nick_name = prompt("请输入你的昵称");
+}
+
+$("#submit").click(function(){
+    var str = $.trim($("input").val());
+    if(str !== ""){
+        var data = {"message": `${nick_name} 说: ${str}`};
+
+        socket.emit("mes", data);
+        $("input").val("");
+    }
+});
+
+$("input").keyup(function(event){
+    var key = event.keyCode;
+    var str = $.trim($("input").val());
+    if(key === 13 && str !== ""){
+        $("#submit").click();
+    }
+});
